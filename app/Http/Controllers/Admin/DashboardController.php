11c91eadd7e22165ff6bace8f01de3a462f4086f
2151796 <2151796@slu.edu.ph>
2018-03-14 11:50:44 +0800

    added notifications on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Response;
+use App\Suggestion;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Ordinance;
 use App\Resolution;
-
+use Carbon;
 class DashboardController extends Controller
 {
     /**
@@ -14,6 +16,8 @@ class DashboardController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index() {
-        return view('admin.dashboard.index');
+        $suggestions = Suggestion::Where('created_at','>', Carbon\Carbon::now()->subDays(4))->get();
+        $responses = Response::Where('created_at','>', Carbon\Carbon::now()->subDays(4))->get();
+        return view('admin.dashboard.index')->with('suggestions', $suggestions)->with('responses', $responses);
     }
 }
