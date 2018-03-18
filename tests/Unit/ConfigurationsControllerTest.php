2c6cacc939270d29f09a4c5f15347aaeaf29fe33
Anne Zheng <maryannezheng@gmail.com>
2018-03-18 18:33:11 +0800

    Test config

---
+++ tests/Unit/ConfigurationsControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 3/6/2018
+ * Time: 7:19 PM
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class ConfigurationsControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
