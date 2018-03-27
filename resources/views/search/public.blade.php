3135c0085582a7b181881f6a9854dd0ef4fd7cf0
seanyboyswag <2150218@slu.edu.ph>
2018-03-27 22:42:51 +0800

    Added: Data tables format for Facebook Comments

--- resources/views/search/public.blade.php
+++ resources/views/search/public.blade.php
@@ -11,8 +11,6 @@
 @endsection
 
 @section('scripts')
-    {{--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.css"/>--}}
-    {{--<script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>--}}
     <script type="text/javascript" src="/DataTables/datatables.min.js"></script>
     <script>
         $(document).ready(function () {
