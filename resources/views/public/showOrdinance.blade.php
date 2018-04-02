fb48db9f05dd3850b92a4f31399c3e7d287a7c30
DeanEarl <deanfailed@gmail.com>
2018-04-02 16:06:03 +0800

    fix spacing of td tags

--- resources/views/public/showOrdinance.blade.php
+++ resources/views/public/showOrdinance.blade.php
@@ -84,6 +84,7 @@
                                                 @foreach($ordinance->updateReport()->where('is_deleted', 0)->get() as $updateReport)
                                                     <tr>
                                                         <td>{{$updateReport->pdf_file_name}}</td>
+                                                        <td></td>
                                                         <td>
                                                             <a href="/downloadPDF/updatereports/{{$updateReport->pdf_file_name}}"
                                                                class="btn btn-xs btn-primary btn-equal-width pull-right">
